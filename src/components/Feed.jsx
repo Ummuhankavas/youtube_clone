690b55b59d3cb7704dbcb3761492314ecc154b97
UmmuhanKavas <ummuhankavas@gmail.com>
2023-01-30 10:25:37 +0100

    feed created index.js updated

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import {useState, useEffect }from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import { SideBar, Videos } from '../components';
+
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
 
-export default Feed
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box sx={{ height: { sx: 'auto', md: '92vh'  }, borderRight: '1px solid #3d3d3d', px: {sx:0, md: 2 }}}>
+        <SideBar/>
+        
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+          Copyright © 2023 Ümmühan KAVAS
+        </Typography>
+        
+      </Box>
+
+      <Box p={2} sx={{overflow: 'auto', height: '90vh', flex: 2}}>
+        <Typography variant='h4'
+        fontWeight='bold' mb={2} sx={{color:'white'}}>
+        New <span style={{ color: '#F31503'}}>videos</span>
+        </Typography>
+
+        <Videos videos={[]}/>
+      </Box>
+
+     
+    </Stack>
+  );
+};
+
+export default Feed;
